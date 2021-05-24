762b1af6219b341f28d30ecc8ae8dd41cacc16db
gridbugx <84717331+gridbugx@users.noreply.github.com>
2021-05-24 12:28:51 -0400

    product description injected

---
+++ src/app/product.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { Http,Response } from '@angular/http'
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+@Injectable()
+export class ProductService {
+  _albumUrl: string = '../assets/album.json'
+
+  constructor(private _http: Http) { }
+
+  getAlbum(id:number) : Observable<any>{
+    return this._http.get(this._albumUrl).pipe(map((response: Response) => response.json()))  // #9
+  }
+}
