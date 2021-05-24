aee1b8fe3cc22c10f713ed621fa73a7e7473a379
gridbugx <84717331+gridbugx@users.noreply.github.com>
2021-05-24 13:11:10 -0400

    but it works

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Http,Response } from '@angular/http'
 import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import 'rxjs/add/operator/map';
 
 @Injectable()
 export class ProductService {
@@ -9,8 +9,9 @@ export class ProductService {
 
   constructor(private _http: Http) { }
 
-  //getAlbum(id:number) : Observable<any>{
-  getAlbum(id:number) {
-      return this._http.get(this._albumUrl).pipe(map((response: Response) => response.json()))  // #9
+  getAlbum(id:number) : Observable<any> {
+    //console.log('service returning ', this._http.get(this._albumUrl).pipe(map((response: Response) => response.json())) )
+
+    return this._http.get(this._albumUrl).map((response: Response) => response.json())  // #9
   }
 }
