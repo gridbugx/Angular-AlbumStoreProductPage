af6beecef5846bcb9e11920688e55d23fddfa37d
gridbugx <84717331+gridbugx@users.noreply.github.com>
2021-05-24 14:24:19 -0400

    part 4 almost there

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
-import { Http,Response } from '@angular/http'
 import { Observable } from 'rxjs';
 import 'rxjs/add/operator/map';
+import { Album } from './album';
+import { Http,Response } from '@angular/http'
 
 @Injectable()
 export class ProductService {
@@ -9,8 +10,11 @@ export class ProductService {
 
   constructor(private _http: Http) { }
 
-  getAlbum(id: number) {
-    return this._http.get(this._albumUrl).map((response) => response.json());
-  }
+  albumInfo: Observable<Album> = null
+
+  //getAlbum(id: number) :Observable<Album> {
+  getAlbum(id: number):Observable<Album> {
+      return this._http.get(this._albumUrl).map((response) => response.json()) as Observable<Album>;
+    }
 
 }
